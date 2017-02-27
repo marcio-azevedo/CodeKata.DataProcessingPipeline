6c1785609ef9d5b39f471c5f428e61f73d23e056
Márcio Azevedo <marcio.azevedo@basecone.nl>
2017-02-27 17:42:51 +0000

    Updated release module.

--- src/Infrastructure.Web/Modules/ReleasesModule.cs
+++ src/Infrastructure.Web/Modules/ReleasesModule.cs
@@ -2,6 +2,7 @@
 
 namespace Infrastructure.Web.Modules
 {
+    // check https://github.com/NancyFx/Nancy/wiki/Exploring-the-nancy-module
     public class ReleasesModule : NancyModule
     {
         public ReleasesModule() : base("/releases")
